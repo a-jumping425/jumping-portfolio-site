7ac1da476929a80e5518a45573b51edc8a23389f
Jumping <a.jumping425@gmail.com>
2017-05-29 12:23:02 +0700

    Change portfolio_categories table

--- database/migrations/2017_05_23_031538_portfolio_categories_table.php
+++ database/migrations/2017_05_23_031538_portfolio_categories_table.php
@@ -17,7 +17,7 @@ class PortfolioCategoriesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('slug');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->bigInteger('ordering');
         });
 
