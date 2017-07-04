a3d8fe935de8fcb95086b2843226ee5d18ccde96
Jumping <a.jumping425@gmail.com>
2017-07-04 16:49:48 +0700

    Show login form for admin dashboard.

--- resources/views/backend/auth/login.blade.php
+++ resources/views/backend/auth/login.blade.php
@@ -1,68 +1,71 @@
-@extends('layouts.app')
+@extends('backend.template_auth')
+
+@section('title', 'Admin Login')
+
+@section('page_level_plugins_css')
+    <link href="/assets/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/assets/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css"/>
+@endsection
+
+@section('page_level_css')
+    <link href="/assets/pages/css/login.css" rel="stylesheet" type="text/css"/>
+@endsection
+
+@section('page_level_plugins_js')
+    <script src="/assets/global/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/jquery-validation/js/additional-methods.min.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/select2/js/select2.full.min.js" type="text/javascript"></script>
+@endsection
+
+@section('page_level_js')
+    <script src="/assets/pages/scripts/login.js" type="text/javascript"></script>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    <!-- BEGIN LOGIN FORM -->
+    <form class="login-form" action="index.html" method="post">
+        <h3 class="form-title font-green">Sign In</h3>
+        <div class="alert alert-danger display-hide">
+            <button class="close" data-close="alert"></button>
+            <span> Enter any username and password. </span>
         </div>
-    </div>
-</div>
+        <div class="form-group">
+            <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
+            <label class="control-label visible-ie8 visible-ie9">Username</label>
+            <input class="form-control form-control-solid placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username" /> </div>
+        <div class="form-group">
+            <label class="control-label visible-ie8 visible-ie9">Password</label>
+            <input class="form-control form-control-solid placeholder-no-fix" type="password" autocomplete="off" placeholder="Password" name="password" /> </div>
+        <div class="form-actions">
+            <button type="submit" class="btn green uppercase">Login</button>
+            <label class="rememberme check mt-checkbox mt-checkbox-outline">
+                <input type="checkbox" name="remember" value="1" />Remember
+                <span></span>
+            </label>
+            <a href="{{ url('/password/reset') }}" id="forget-password" class="forget-password">Forgot Password?</a>
+        </div>
+        <div class="login-options">
+            <h4>Or login with</h4>
+            <ul class="social-icons">
+                <li>
+                    <a class="social-icon-color facebook" data-original-title="facebook" href="javascript:;"></a>
+                </li>
+                <li>
+                    <a class="social-icon-color twitter" data-original-title="Twitter" href="javascript:;"></a>
+                </li>
+                <li>
+                    <a class="social-icon-color googleplus" data-original-title="Goole Plus" href="javascript:;"></a>
+                </li>
+                <li>
+                    <a class="social-icon-color linkedin" data-original-title="Linkedin" href="javascript:;"></a>
+                </li>
+            </ul>
+        </div>
+        <div class="create-account">
+            <p>
+                <a href="javascript:;" id="register-btn" class="uppercase">Create an account</a>
+            </p>
+        </div>
+    </form>
+    <!-- END LOGIN FORM -->
 @endsection
