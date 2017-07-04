24c428afa0cef352a25b2ad26a7f2078af6ffde5
Jumping <a.jumping425@gmail.com>
2017-07-04 17:39:52 +0700

    Show forgot password form in admin dashboard.

--- resources/views/backend/auth/passwords/email.blade.php
+++ resources/views/backend/auth/passwords/email.blade.php
@@ -1,46 +1,36 @@
-@extends('layouts.app')
+@extends('backend.template_auth')
+
+@section('title', 'Admin Login')
+
+@section('page_level_plugins_css')
+    <link href="/assets/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/assets/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css"/>
+@endsection
+
+@section('page_level_css')
+    <link href="/assets/pages/css/login.css" rel="stylesheet" type="text/css"/>
+@endsection
+
+@section('page_level_plugins_js')
+    <script src="/assets/global/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/jquery-validation/js/additional-methods.min.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/select2/js/select2.full.min.js" type="text/javascript"></script>
+@endsection
+
+@section('page_level_js')
+    <script src="/assets/pages/scripts/login.js" type="text/javascript"></script>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    <!-- BEGIN FORGOT PASSWORD FORM -->
+    <form class="forget-form" action="index.html" method="post">
+        <h3 class="font-green">Forget Password ?</h3>
+        <p> Enter your e-mail address below to reset your password. </p>
+        <div class="form-group">
+            <input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Email" name="email" /> </div>
+        <div class="form-actions">
+            <button type="submit" class="btn btn-success uppercase pull-right">Submit</button>
         </div>
-    </div>
-</div>
+    </form>
+    <!-- END FORGOT PASSWORD FORM -->
 @endsection
